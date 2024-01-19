0d488626f72149052b8495146b92ce144f1bd7bb
oops-moment <prisha.kumar@students.iiit.ac.in>
2024-01-19 22:23:37 +0530

    assignment done,3 left to d

--- Assignment 1/Assignment1_Final/2MPI_Input.cpp
+++ Assignment 1/Assignment1_Final/2MPI_Input.cpp
@@ -23,7 +23,6 @@ int main(int argc, char *argv[])
     int rank, processes;
     int N;
     MPI_Status status;
-
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &processes);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -34,8 +33,9 @@ int main(int argc, char *argv[])
         start = MPI_Wtime();
         cin >> N;
         MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        int length = N * N;
         vector<int> vec(N * N);
-        for (int i = 0; i < (N * N); i++)
+        for (int i = 0; i < length; i++)
         {
             cin >> vec[i];
             if (vec[i] == -1)
@@ -43,6 +43,8 @@ int main(int argc, char *argv[])
                 vec[i] = 1e9;
             }
         }
+
+        // If the number of processes is 1 then no need to run the parallel algorithm since it gonna work same as the simple algorithm
 
         if (processes == 1)
         {
@@ -67,6 +69,7 @@ int main(int argc, char *argv[])
             MPI_Finalize();
             return 0;
         }
+
         int size = vec.size();
         MPI_Bcast(vec.data(), size, MPI_INT, 0, MPI_COMM_WORLD);
         vector<int> result(3);
@@ -76,9 +79,20 @@ int main(int argc, char *argv[])
         {
             MPI_Recv(result.data(), 3, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
             if (status.MPI_TAG == 2)
+            {
                 disable++;
+            }
             else
-                vec[result[1] * N + result[2]] = min(vec[result[1] * N + result[2]], result[0]);
+            {
+                if (vec[result[1] * N + result[2]] >= result[0])
+                {
+                    vec[result[1] * N + result[2]] = result[0];
+                }
+                else
+                {
+                    vec[result[1] * N + result[2]] = vec[result[1] * N + result[2]];
+                }
+            }
         } while (disable < total_recvs);
         end = MPI_Wtime();
         cout << "\n";
@@ -94,31 +108,33 @@ int main(int argc, char *argv[])
 
         int remainder = N % (processes - 1);
         int slices = (N - remainder) / (processes - 1);
-        if ((rank + 1) != processes)
+        if (rank != processes - 1)
+        {
             remainder = 0;
+        }
+        int start_index = slices * (rank - 1);
+        int end_index = slices * (rank - 1) + slices;
+        vector<int> result(3);
 
-        // only the last process does the extra job that has to be done
-
-        vector<int> out(3);
-
-        for (int k = (slices * (rank - 1)); k < ((slices * (rank - 1)) + slices + remainder); k++)
+        for (int k = (slices * (rank - 1)); k < (end_index + remainder); k++)
         {
             for (int i = 0; i < N; i++)
             {
                 for (int j = 0; j < N; j++)
                 {
-                    if ((vec[i * N + k] + vec[k * N + j] < vec[i * N + j]))
+                    int possible_distance = vec[i * N + k] + vec[k * N + j];
+                    if (possible_distance < vec[i * N + j])
                     {
-                        vec[i * N + j] = vec[i * N + k] + vec[k * N + j];
-                        out[0] = vec[i * N + j];
-                        out[1] = i;
-                        out[2] = j;
-                        MPI_Send(out.data(), 3, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                        vec[i * N + j] = possible_distance;
+                        result[0] = vec[i * N + j];
+                        result[1] = i;
+                        result[2] = j;
+                        MPI_Send(result.data(), 3, MPI_INT, 0, 0, MPI_COMM_WORLD);
                     }
                 }
             }
         }
-        MPI_Send(out.data(), 3, MPI_INT, 0, 2, MPI_COMM_WORLD);
+        MPI_Send(result.data(), 3, MPI_INT, 0, 2, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
