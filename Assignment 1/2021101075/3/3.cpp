8e64bac77b023adccb6d6909ff64446d37946c52
oops-moment <prisha.kumar@students.iiit.ac.in>
2024-01-26 11:55:57 +0530

    more changed

--- Assignment 1/2021101075/3/3.cpp
+++ Assignment 1/2021101075/3/3.cpp
@@ -162,39 +162,41 @@ int main(int argc, char *argv[])
         nRowsLocal += nRows % size;
     }
 
-    int nrowsLocalWithGhost = nRowsLocal + 2; // 12
-    int ncolswithGhost = nCols + 2;           // 12
+    int nrowsLocalWithGhost = nRowsLocal + 2;
+    int ncolswithGhost = nCols + 2;
 
-    // Now we need 2 arrays, one for the current and one for the future
-    vector<vector<int>> currentGrid(nrowsLocalWithGhost, vector<int>(ncolswithGhost, 0)); // 12X12
-    vector<vector<int>> futureGrid(nrowsLocalWithGhost, vector<int>(ncolswithGhost, 0));  // 12X12
+    vector<vector<int>> currentGrid(nrowsLocalWithGhost, vector<int>(ncolswithGhost, 0));
+    vector<vector<int>> futureGrid(nrowsLocalWithGhost, vector<int>(ncolswithGhost, 0));
+
+    int up = size - 1;
+    if (rank != 0)
+    {
+        up = rank - 1;
+    }
+    int down = 0;
+    if (rank != (size - 1))
+    {
+        down = rank + 1;
+    }
 
     for (int i = 1 + rank * (nRows / size); i <= nRowsLocal + rank * (nRows / size); i++)
     {
         for (int j = 1; j <= nCols; j++)
         {
-            currentGrid[i - rank * (nRows / size)][j] = newArray[(i - 1) * nCols + j - 1];
+            int roww = i - rank * (nRows / size);
+            int indexx = (i - 1) * nCols + (j - 1);
+
+            currentGrid[roww][j] = newArray[indexx];
         }
     }
-
-    // Now we would need two neighbors: one is the upper neighbor and the other is the lower neighbor
-    int upperNeighbor = (rank == 0) ? size - 1 : rank - 1;
-    int lowerNeighbor = (rank == size - 1) ? 0 : rank + 1;
-
     // NITERATION LOOP
     for (int iter = 0; iter < iterations; iter++)
     {
-        // send top row above
-        MPI_Send(&currentGrid[1][0], ncolswithGhost, MPI_INT, upperNeighbor, 0, MPI_COMM_WORLD);
+        MPI_Send(&currentGrid[1][0], ncolswithGhost, MPI_INT, up, 0, MPI_COMM_WORLD);
+        MPI_Send(&currentGrid[nRowsLocal][0], ncolswithGhost, MPI_INT, down, 0, MPI_COMM_WORLD);
 
-        // send bottom row below
-        MPI_Send(&currentGrid[nRowsLocal][0], ncolswithGhost, MPI_INT, lowerNeighbor, 0, MPI_COMM_WORLD);
-
-        // receive the bottom row from below
-        MPI_Recv(&currentGrid[nRowsLocal + 1][0], ncolswithGhost, MPI_INT, lowerNeighbor, 0, MPI_COMM_WORLD, &status);
-
-        // receive the top row from above
-        MPI_Recv(&currentGrid[0][0], ncolswithGhost, MPI_INT, upperNeighbor, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&currentGrid[nRowsLocal + 1][0], ncolswithGhost, MPI_INT, down, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&currentGrid[0][0], ncolswithGhost, MPI_INT, up, 0, MPI_COMM_WORLD, &status);
 
         if (rank == 0)
         {
@@ -222,7 +224,6 @@ int main(int argc, char *argv[])
         {
 
             printMatrix(currentGrid, nRowsLocal, nCols, iter);
-
             for (int srcrank = 1; srcrank < size; srcrank++)
             {
                 int torecv = nRows / size;
