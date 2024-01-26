e4096b8473fd12379e1553c470920e5b2c7f1073
oops-moment <prisha.kumar@students.iiit.ac.in>
2024-01-26 12:33:10 +0530

    wrking

--- Assignment 1/2021101075/3/3.cpp
+++ Assignment 1/2021101075/3/3.cpp
@@ -12,19 +12,19 @@ void WriteToFile(int size, double time)
     myfile.close();
 }
 
-void printMatrix(vector<vector<int>> &currentGrid, int nRowsLocal, int nCols, int iter)
+void printMatrix(vector<int> &currentGrid, int nRowsLocal, int nCols, int iter)
 {
     cout << "Iteration " << iter << "\n";
     for (int i = 1; i <= nRowsLocal; i++)
     {
         for (int j = 1; j <= nCols; j++)
         {
-            cout << currentGrid[i][j] << " ";
+            cout << currentGrid[i * (nCols + 2) + j] << " ";
         }
         cout << "\n";
     }
 }
-void performIteration(vector<vector<int>> &currentGrid, vector<vector<int>> &futureGrid, int nRowsLocal, int nCols)
+void performIteration(vector<int> &currentGrid, vector<int> &futureGrid, int nRowsLocal, int nCols)
 {
     const int ALIVE = 1;
     const int DEAD = 0;
@@ -38,44 +38,44 @@ void performIteration(vector<vector<int>> &currentGrid, vector<vector<int>> &fut
             {
                 for (int jj = j - 1; jj <= j + 1; jj++)
                 {
-                    if ((ii != i || jj != j) && currentGrid[ii][jj] == ALIVE)
+                    if ((ii != i || jj != j) && currentGrid[ii * (nCols + 2) + jj] == ALIVE)
                         alive++;
                 }
             }
 
-            if (currentGrid[i][j] == ALIVE)
+            if (currentGrid[i * (nCols + 2) + j] == ALIVE)
             {
                 if (alive < 2)
                 {
-                    futureGrid[i][j] = DEAD;
+                    futureGrid[i * (nCols + 2) + j] = DEAD;
                 }
                 else if (alive == 2 || alive == 3)
                 {
-                    futureGrid[i][j] = ALIVE;
+                    futureGrid[i * (nCols + 2) + j] = ALIVE;
                 }
                 else if (alive > 3)
                 {
-                    futureGrid[i][j] = DEAD;
+                    futureGrid[i * (nCols + 2) + j] = DEAD;
                 }
             }
             else
             {
                 if (alive == 3)
                 {
-                    futureGrid[i][j] = ALIVE;
+                    futureGrid[i * (nCols + 2) + j] = ALIVE;
                 }
             }
         }
     }
 }
 
-void updateGrid(vector<vector<int>> &currentGrid, vector<vector<int>> &futureGrid, int nRowsLocal, int nCols)
+void updateGrid(vector<int> &currentGrid, vector<int> &futureGrid, int nRowsLocal, int nCols)
 {
     for (int i = 1; i <= nRowsLocal; i++)
     {
         for (int j = 1; j <= nCols; j++)
         {
-            currentGrid[i][j] = futureGrid[i][j];
+            currentGrid[i * (nCols + 2) + j] = futureGrid[i * (nCols + 2) + j];
         }
     }
 }
@@ -117,22 +117,22 @@ int main(int argc, char *argv[])
     if (size == 1)
     {
         iterations -= 1;
-        vector<vector<int>> currentGrid(nRows + 2, vector<int>(nCols + 2, 0));
-        vector<vector<int>> futureGrid(nRows + 2, vector<int>(nCols + 2, 0));
+        vector<int> currentGrid1D(((nRows + 2) * (nCols + 2)), 0);
+        vector<int> futureGrid1D(((nRows + 2) * (nCols + 2)), 0);
 
         for (int i = 1; i <= nRows; i++)
         {
             for (int j = 1; j <= nCols; j++)
             {
-                currentGrid[i][j] = newArray[(i - 1) * nCols + j - 1];
+                currentGrid1D[i * (nCols + 2) + j] = newArray[(i - 1) * nCols + j - 1];
             }
         }
 
         for (int iter = 0; iter < iterations; iter++)
         {
-            performIteration(currentGrid, futureGrid, nRows, nCols);
-            updateGrid(currentGrid, futureGrid, nRows, nCols);
-            printMatrix(currentGrid, nRows, nCols, iter);
+            performIteration(currentGrid1D, futureGrid1D, nRows, nCols);
+            updateGrid(currentGrid1D, futureGrid1D, nRows, nCols);
+            printMatrix(currentGrid1D, nRows, nCols, iter);
         }
 
         double end_time = MPI_Wtime();
@@ -165,8 +165,8 @@ int main(int argc, char *argv[])
     int nrowsLocalWithGhost = nRowsLocal + 2;
     int ncolswithGhost = nCols + 2;
 
-    vector<vector<int>> currentGrid(nrowsLocalWithGhost, vector<int>(ncolswithGhost, 0));
-    vector<vector<int>> futureGrid(nrowsLocalWithGhost, vector<int>(ncolswithGhost, 0));
+    vector<int> currentGrid1D(nrowsLocalWithGhost * ncolswithGhost, 0);
+    vector<int> futureGrid1D(nrowsLocalWithGhost * ncolswithGhost, 0);
 
     int up = size - 1;
     if (rank != 0)
@@ -183,33 +183,33 @@ int main(int argc, char *argv[])
     {
         for (int j = 1; j <= nCols; j++)
         {
-            int roww = i - rank * (nRows / size);
-            int indexx = (i - 1) * nCols + (j - 1);
+            int indexx1 = (i - rank * (nRows / size)) * (ncolswithGhost) + j;
+            int indexx2 = (i - 1) * nCols + (j - 1);
 
-            currentGrid[roww][j] = newArray[indexx];
+            currentGrid1D[indexx1] = newArray[indexx2];
         }
     }
     // NITERATION LOOP
     for (int iter = 0; iter < iterations; iter++)
     {
-        MPI_Send(&currentGrid[1][0], ncolswithGhost, MPI_INT, up, 0, MPI_COMM_WORLD);
-        MPI_Send(&currentGrid[nRowsLocal][0], ncolswithGhost, MPI_INT, down, 0, MPI_COMM_WORLD);
+        MPI_Send(&currentGrid1D[ncolswithGhost], ncolswithGhost, MPI_INT, up, 0, MPI_COMM_WORLD);
+        MPI_Send(&currentGrid1D[nRowsLocal * ncolswithGhost], ncolswithGhost, MPI_INT, down, 0, MPI_COMM_WORLD);
 
-        MPI_Recv(&currentGrid[nRowsLocal + 1][0], ncolswithGhost, MPI_INT, down, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv(&currentGrid[0][0], ncolswithGhost, MPI_INT, up, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&currentGrid1D[(nRowsLocal + 1) * ncolswithGhost], ncolswithGhost, MPI_INT, down, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&currentGrid1D[0], ncolswithGhost, MPI_INT, up, 0, MPI_COMM_WORLD, &status);
 
         if (rank == 0)
         {
             for (int i = 0; i < ncolswithGhost; i++)
             {
-                currentGrid[0][i] = 0;
+                currentGrid1D[i] = 0;
             }
         }
         if (rank == size - 1)
         {
             for (int i = 0; i < ncolswithGhost; i++)
             {
-                currentGrid[nRowsLocal + 1][i] = 0;
+                currentGrid1D[(nRowsLocal + 1) * ncolswithGhost + i] = 0;
             }
         }
 
@@ -217,13 +217,13 @@ int main(int argc, char *argv[])
         {
             for (int row = 1; row <= nRowsLocal; row++)
             {
-                MPI_Send(&currentGrid[row][1], nCols, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                MPI_Send(&currentGrid1D[row * ncolswithGhost + 1], nCols, MPI_INT, 0, 0, MPI_COMM_WORLD);
             }
         }
         if (rank == 0)
         {
 
-            printMatrix(currentGrid, nRowsLocal, nCols, iter);
+            printMatrix(currentGrid1D, nRowsLocal, nCols, iter);
             for (int srcrank = 1; srcrank < size; srcrank++)
             {
                 int torecv = nRows / size;
@@ -243,8 +243,8 @@ int main(int argc, char *argv[])
                 }
             }
         }
-        performIteration(currentGrid, futureGrid, nRowsLocal, nCols);
-        updateGrid(currentGrid, futureGrid, nRowsLocal, nCols);
+        performIteration(currentGrid1D, futureGrid1D, nRowsLocal, nCols);
+        updateGrid(currentGrid1D, futureGrid1D, nRowsLocal, nCols);
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
