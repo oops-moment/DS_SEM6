a2432d8859778c5346270f20a7ae8a5d91a4493d
oops-moment <prisha.kumar@students.iiit.ac.in>
2024-02-23 19:27:28 +0530

    q2 chal gyaaaa

--- .vscode/2021101075/q2/runner-script.sh
+++ .vscode/2021101075/q2/runner-script.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Check if the correct number of arguments are provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <LOCAL_INPUT_DIR> <HDFS_INPUT_DIR> <HDFS_OUTPUT_DIR>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <HADOOP_JARR> <LOCAL_INPUT_DIR> <HDFS_INPUT_DIR> <HDFS_OUTPUT_DIR>"
     exit 1
 fi
 
 # Assign arguments to variables
-LOCAL_INPUT_DIR=$1
-HDFS_INPUT_DIR=$2
-HDFS_OUTPUT_DIR=$3
-HDFS_TEMP_DIR=/helper
+HADOOP_JARR=$1
+LOCAL_INPUT_DIR=$2
+HDFS_INPUT_DIR=$3
+HDFS_OUTPUT_DIR=$4
+HDFS_TEMP_DIR=/helper_2021101075
 
 # Function to create HDFS directory if it doesn't exist
 create_hdfs_directory() {
@@ -31,21 +32,20 @@ hdfs dfs -put "$LOCAL_INPUT_DIR" "$HDFS_INPUT_DIR/"
 # Specify the mapper and reducer scripts for the first stage
 MAPPER_SCRIPT0=mapper0.py
 REDUCER_SCRIPT0=reducer0.py
-
 # Run the first MapReduce job using mapred streaming
 mapred streaming \
     -files "$MAPPER_SCRIPT0","$REDUCER_SCRIPT0" \
-    -input "$HDFS_INPUT_DIR" \
+    -input "$HDFS_INPUT_DIR/input.txt" \
     -output "$HDFS_TEMP_DIR" \
     -mapper "$MAPPER_SCRIPT0" \
     -reducer "$REDUCER_SCRIPT0" \
     -numReduceTasks 3
 
-# Specify the mapper and reducer scripts for the second stage
+# Run the second MapReduce job using mapred streaming
+
 MAPPER_SCRIPT1=mapper1.py
 REDUCER_SCRIPT1=reducer1.py
 
-# Run the second MapReduce job using mapred streaming
 mapred streaming \
     -files "$MAPPER_SCRIPT1","$REDUCER_SCRIPT1" \
     -input "$HDFS_TEMP_DIR/part-*" \
@@ -54,12 +54,13 @@ mapred streaming \
     -reducer "$REDUCER_SCRIPT1" \
     -numReduceTasks 3
 
-# Display the contents of the output files
-hdfs dfs -cat "$HDFS_OUTPUT_DIR/part-*" | sort
 
 # Delete the input and output files
 hdfs dfs -rm -r "$HDFS_INPUT_DIR"
-hdfs dfs -rm -r "$HDFS_OUTPUT_DIR"
+# hdfs dfs -rm -r "$HDFS_OUTPUT_DIR"
 hdfs dfs -rm -r "$HDFS_TEMP_DIR"
+
+# display the output
+hdfs dfs -cat "$HDFS_OUTPUT_DIR/*" | sort
 
 echo "Hadoop job completed successfully!"
